841b098cc755970f5a0d6f1bfe7725efb80f62ab
Jakin Chan <JakinChan200@gmail.com>
2025-01-07 01:27:20 -0800

    Updated transpose and count_empty functions

--- 2048.cpp
+++ 2048.cpp
@@ -41,38 +41,35 @@ int add(int num,  int num2){
 #undef min
 #endif
 
-// Transpose rows/columns in a board:
-//   0123       048c
-//   4567  -->  159d
-//   89ab       26ae
-//   cdef       37bf
-static inline board_t transpose(board_t x)
-{
-    board_t a1 = x & 0xF0F00F0FF0F00F0FULL;
-    board_t a2 = x & 0x0000F0F00000F0F0ULL;
-    board_t a3 = x & 0x0F0F00000F0F0000ULL;
-    board_t a = a1 | (a2 << 12) | (a3 >> 12);
-    board_t b1 = a & 0xFF00FF0000FF00FFULL;
-    board_t b2 = a & 0x00FF00FF00000000ULL;
-    board_t b3 = a & 0x00000000FF00FF00ULL;
-    return b1 | (b2 >> 24) | (b3 << 24);
+//flip the rows and cols
+static inline board_t transpose(board_t board){
+    board_t temp = 0x0ULL;
+    temp |= unpack_col(board >>  0 & ROW_MASK);
+    temp |= unpack_col(board >> 16 & ROW_MASK) << 4;
+    temp |= unpack_col(board >> 32 & ROW_MASK) << 8;
+    temp |= unpack_col(board >> 48 & ROW_MASK) << 12;
+
+    return temp;
 }
 
 // Count the number of empty positions (= zero nibbles) in a board.
 // Precondition: the board cannot be fully empty.
-static int count_empty(board_t x)
-{
-    x |= (x >> 2) & 0x3333333333333333ULL;
+static int count_empty(board_t x){
+    if(x == 0){
+        return 16;
+    }
+
+    //https://stackoverflow.com/questions/38225571/count-number-of-zero-nibbles-in-an-unsigned-64-bit-integer
+    //Gather 1s into the 1s position of each 4
     x |= (x >> 1);
+    x |= (x >> 2);
+    //Zero the other positions and flip the value of the 1s position
     x = ~x & 0x1111111111111111ULL;
-    // At this point each nibble is:
-    //  0 if the original nibble was non-zero
-    //  1 if the original nibble was zero
-    // Next sum them all
+
     x += x >> 32;
     x += x >> 16;
     x += x >>  8;
-    x += x >>  4; // this can overflow to the next nibble if there were 16 empty positions
+    x += x >>  4; 
     return x & 0xf;
 }
 
@@ -389,7 +386,6 @@ static float score_move_node(eval_state &state, board_t board, float cprob) {
 
 //If the move means the board does not change, return 0, otherwise call score_tilechoose_node with the move
 static float _score_toplevel_move(eval_state &state, board_t board, int move) {
-    //int maxrank = get_max_rank(board);
     board_t newboard = execute_move(move, board);
 
     if(board == newboard)
@@ -401,20 +397,10 @@ static float _score_toplevel_move(eval_state &state, board_t board, int move) {
 //Decides the depth and calls _score_toplevel_move
 float score_toplevel_move(board_t board, int move) {
     float res;
-    // struct timeval start, finish;
-    double elapsed;
     eval_state state;
     state.depth_limit = std::max(3, count_distinct_tiles(board) - 2);
 
-    // gettimeofday(&start, NULL);
     res = _score_toplevel_move(state, board, move);
-    // gettimeofday(&finish, NULL);
-
-    // elapsed = (finish.tv_sec - start.tv_sec);
-    // elapsed += (finish.tv_usec - start.tv_usec) / 1000000.0;
-
-    // printf("Move %d: result %f: eval'd %ld moves (%d cache hits, %d cache size) in %.2f seconds (maxdepth=%d)\n", move, res,
-    //     state.moves_evaled, state.cachehits, (int)state.trans_table.size(), elapsed, state.maxdepth);
 
     return res;
 }
@@ -440,105 +426,3 @@ int find_best_move(board_t board) {
 
     return bestmove;
 }
-
-// int ask_for_move(board_t board) {
-//     int move;
-//     char validstr[5];
-//     char *validpos = validstr;
-
-//     printBoard(board);
-
-//     for(move=0; move<4; move++) {
-//         if(execute_move(move, board) != board)
-//             *validpos++ = "UDLR"[move];
-//     }
-//     *validpos = 0;
-//     if(validpos == validstr)
-//         return -1;
-
-//     while(1) {
-//         char movestr[64];
-//         const char *allmoves = "UDLR";
-
-//         printf("Move [%s]? ", validstr);
-
-//         if(!fgets(movestr, sizeof(movestr)-1, stdin))
-//             return -1;
-
-//         if(!strchr(validstr, toupper(movestr[0]))) {
-//             printf("Invalid move.\n");
-//             continue;
-//         }
-
-//         return strchr(allmoves, toupper(movestr[0])) - allmoves;
-//     }
-// }
-
-// /* Playing the game */
-// static board_t draw_tile() {
-//     return (unif_random(10) < 9) ? 1 : 2;
-// }
-
-// static board_t insert_tile_rand(board_t board, board_t tile) {
-//     int index = unif_random(count_empty(board));
-//     board_t tmp = board;
-//     while (true) {
-//         while ((tmp & 0xf) != 0) {
-//             tmp >>= 4;
-//             tile <<= 4;
-//         }
-//         if (index == 0) break;
-//         --index;
-//         tmp >>= 4;
-//         tile <<= 4;
-//     }
-//     return board | tile;
-// }
-
-// static board_t initial_board() {
-//     board_t board = draw_tile() << (4 * unif_random(16));
-//     return insert_tile_rand(board, draw_tile());
-// }
-
-// void play_game(get_move_func_t get_move) {
-//     board_t board = initial_board();
-//     int moveno = 0;
-//     int scorepenalty = 0; // "penalty" for obtaining free 4 tiles
-
-//     while(1) {
-//         int move;
-//         board_t newboard;
-
-//         for(move = 0; move < 4; move++) {
-//             if(execute_move(move, board) != board)
-//                 break;
-//         }
-//         if(move == 4)
-//             break; // no legal moves
-
-//         printf("\nMove #%d, current score=%.0f\n", ++moveno, score_board(board) - scorepenalty);
-
-//         move = get_move(board);
-//         if(move < 0)
-//             break;
-
-//         newboard = execute_move(move, board);
-//         if(newboard == board) {
-//             printf("Illegal move!\n");
-//             moveno--;
-//             continue;
-//         }
-
-//         board_t tile = draw_tile();
-//         if (tile == 2) scorepenalty += 4;
-//         board = insert_tile_rand(newboard, tile);
-//     }
-
-//     printBoard(board);
-//     printf("\nGame over. Your score is %.0f. The highest rank you achieved was %d.\n", score_board(board) - scorepenalty, get_max_rank(board));
-// }
-
-// int main() {
-//     init_tables();
-//     play_game(find_best_move);
-// }
